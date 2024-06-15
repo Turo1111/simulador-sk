26702d4f86f1da870c8e9327da706eb1072bb9ab
matias-note <matiaszurita1998gmail.com>
2024-06-15 18:04:13 -0300

    correccion font 7

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,23 +1,17 @@
-'use client'
 import { Inter } from "next/font/google";
 import "./globals.css";
-import { createGlobalStyle } from "styled-components";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const GlobalStyle = createGlobalStyle`
-  body {
-    font-family: 'Inter', sans-serif;
-  }
-`;
+export const metadata = {
+  title: "SIMULADOR"
+};
+
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <GlobalStyle />
-        {children}
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
